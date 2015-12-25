dfa714353e6be29701d06cfed619558a733b3ce9
Joe Sak <joe@joesak.com>
2015-12-25 13:02:00 -0500

    Make post its own file

--- lib/dbc/blog.rb
+++ lib/dbc/blog.rb
@@ -1,4 +1,5 @@
-require "dbc/blog/version"
+require "dbc/version"
+require 'dbc/post'
 
 module Dbc
   class Blog
@@ -18,36 +19,6 @@ module Dbc
 
     def drafted_posts
       @posts.select(&:drafted?).sort_by(&:drafted_at).reverse
-    end
-  end
-
-  class Post
-    attr_reader :title, :body, :published_at, :drafted_at
-
-    def initialize(attrs = {})
-      attrs.each do |k, v|
-        instance_variable_set("@#{k}", v)
-      end
-
-      @published_at = Time.now unless drafted?
-    end
-
-    def drafted?
-      drafted_at
-    end
-
-    def draft!
-      @drafted_at = Time.now
-      @published_at = nil
-    end
-
-    def published?
-      published_at
-    end
-
-    def publish!
-      @published_at = Time.now
-      @drafted_at = nil
     end
   end
 end
---
+++ lib/dbc/post.rb
@@ -0,0 +1,31 @@
+module Dbc
+  class Post
+    attr_reader :title, :body, :published_at, :drafted_at
+
+    def initialize(attrs = {})
+      attrs.each do |k, v|
+        instance_variable_set("@#{k}", v)
+      end
+
+      @published_at = Time.now unless drafted?
+    end
+
+    def drafted?
+      drafted_at
+    end
+
+    def draft!
+      @drafted_at = Time.now
+      @published_at = nil
+    end
+
+    def published?
+      published_at
+    end
+
+    def publish!
+      @published_at = Time.now
+      @drafted_at = nil
+    end
+  end
+end
