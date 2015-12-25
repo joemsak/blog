d473c572c64601ce28da8a63fe5eecc25fcefa07
Joe Sak <joe@joesak.com>
2015-12-25 12:53:06 -0500

    Draft and publish posts

--- lib/dbc/blog.rb
+++ lib/dbc/blog.rb
@@ -1,4 +1,3 @@
-require 'ostruct'
 require "dbc/blog/version"
 
 module Dbc
@@ -8,16 +7,33 @@ module Dbc
     end
 
     def add_post(post_attrs = {})
-      attrs = { published_at: Time.now }.merge(post_attrs)
-      post = Post.new(attrs)
+      post = Post.new(post_attrs)
       @posts << post
       post
     end
 
     def posts
-      @posts.sort_by(&:published_at).reverse
+      @posts.select(&:published?).sort_by(&:published_at).reverse
     end
   end
 
-  class Post < OpenStruct; end
+  class Post
+    attr_reader :title, :body, :published_at, :drafted_at
+
+    def initialize(attrs = {})
+      attrs.each do |k, v|
+        instance_variable_set("@#{k}", v)
+      end
+
+      @published_at = Time.now unless drafted?
+    end
+
+    def drafted?
+      drafted_at
+    end
+
+    def published?
+      published_at
+    end
+  end
 end
--- spec/dbc/blog_spec.rb
+++ spec/dbc/blog_spec.rb
@@ -8,4 +8,14 @@ describe Dbc::Blog do
 
     expect(blog.posts).to eq([second_post, first_post])
   end
+
+  it 'lists only published posts by default' do
+    blog = Dbc::Blog.new
+    published = blog.add_post(title: 'First post', body: 'A great, long post')
+    drafted = blog.add_post(title: 'Second post',
+                            body: 'Another great, long post',
+                            drafted_at: Time.now)
+
+    expect(blog.posts).to eq([published])
+  end
 end
